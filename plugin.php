ec9f75f60158782e079b12ccd6f916405fa6fb52
Mark Daniel Louwe <mrkdnlph@gmail.com>
2020-05-17 15:24:11 +0800

    First commit

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: profile photo upload feature
+Plugin URI:  github.com 
+Description: xx
+Version:     1
+Author:      Mark Daniel Louwe, Danica Niña Louwe
+Author URI:  https://louwewebdesignservices.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
